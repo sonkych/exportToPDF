f98a3c4dd8b0534d4df10c23fb7a4119cd19d19c
DaniilPeterson <daniil.peterson@wemakesoftware.eu>
2024-03-28 21:22:07 +0200

    Allow all connections

--- src/main/java/ee/hcapp/javaservice/config/WebConfig.java
+++ src/main/java/ee/hcapp/javaservice/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowedHeaders("*");
     }
 }
